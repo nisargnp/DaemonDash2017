ee34afa0d05ed1d533e90059f6e22c8a5ae8813c
TechMatt1337 <mattgmb@yahoo.com>
2017-01-28 20:23:58 -0500

    Updated shop

--- DBStuff/Shop.java
+++ DBStuff/Shop.java
@@ -7,39 +7,52 @@ package com.example.matthew.databasetest2;
 public class Shop {
     private int id;
     private String name;
-    private String address;
+    private String products;
+    private String prices;
+    private String reviewer;
+    private String rating;
+    private String stockID;
 
     public Shop() {
 
     }
 
-    public Shop(int id, String name, String address) {
+    public Shop(int id, String name, String products, String prices,
+                String reviewer, String rating, String stockID) {
         this.id = id;
         this.name = name;
-        this.address = address;
+        this.products = products;
+        this.prices = prices;
+        this.reviewer = reviewer;
+        this.rating = rating;
+        this.stockID = stockID;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) { this.id = id; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    public void setProducts(String products) { this.products = products; }
 
-    public int getId() {
-        return id;
-    }
+    public void setPrices(String prices) { this.prices = prices; }
 
-    public String getName() {
-        return name;
-    }
+    public void setReviewer(String reviewer) { this.reviewer = reviewer; }
 
-    public String getAddresss() {
-        return address;
-    }
+    public void setRating(String rating) { this.rating = rating; }
+
+    public void setStockID(String stockID) { this.stockID = stockID; }
+
+    public int getId(int id) { return id; }
+
+    public String getName(String name) { return name; }
+
+    public String getProducts(String products) { return products; }
+
+    public String getPrices(String prices) { return prices; }
+
+    public String getReviewer(String reviewer) { return reviewer; }
+
+    public String getRating(String rating) { return rating; }
+
+    public String getStockID(String stockID) { return stockID; }
 }
