2ff85253172fb4a2260206665b0f52ec4bb7f9e3
Calvin <cholman14@gmail.com>
2017-01-28 22:03:26 -0500

    Added Parser and data.xls

---
+++ app/src/main/java/pl/itraff/androidsample/Parser.java
@@ -0,0 +1,90 @@
+package pl.itraff.androidsample;
+
+import java.io.File;
+import java.io.IOException;
+import jxl.Cell;
+import jxl.CellType;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class Parser {
+	
+	private String inputFile;
+
+	public static void main(String args[]) {
+		Parser p = new Parser("data.xls");
+		try {
+			p.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	//constructor takes in excel file name
+	public Parser (String inputFile) {
+		this.inputFile = inputFile;
+	}
+
+	//parsing function
+	public void read() throws IOException {
+		String name = "", products = "", prices = "", reviewers = "", ratings = "", stockCode = "";
+		
+		File inputWorkbook = new File(inputFile);
+		Workbook w;
+		try {
+			w = Workbook.getWorkbook(inputWorkbook);
+			// Get the first sheet
+			Sheet sheet = w.getSheet(0);
+			// Loop over first 10 column and lines
+
+			for (int j = 1; j < sheet.getRows(); j++) 
+			{
+				
+				for (int i = 0; i < sheet.getColumns(); i++) 
+				{
+					//0 - Name
+					//1 - Product list
+					//2 - Prices
+					//3 - Reviewer
+					//4 - Rating
+					//5 - Stock Symbol
+					Cell cell = sheet.getCell(i, j);
+					
+					if (i == 0)
+						name = cell.getContents();
+					else if (i == 1)
+						products = cell.getContents();
+					else if (i == 2)
+						prices = cell.getContents();
+					else if (i == 3)
+						reviewers = cell.getContents();
+					else if (i == 4)
+						ratings = cell.getContents();
+					else
+						stockCode = cell.getContents();
+				}
+				
+				/*if (products.equals(""))
+					products = null;
+				if (prices.equals(""))
+					prices = null;
+				if (reviewers.equals(""))
+					reviewers = null;
+				if (ratings.equals(""))
+					ratings = null;
+				if (stockCode.equals(""))
+					stockCode = null;*/
+				
+				Shop shop = new Shop(j, name, products, prices, reviewers, ratings, stockCode);
+				System.out.println(shop.toString());
+				
+			}
+		} catch (BiffException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
